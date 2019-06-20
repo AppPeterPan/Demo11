6e7682fb543d42e9eae54a027c805b6f669b5396
peter <apppeterpan@gmail.com>
2019-06-20 23:54:23 +0800

    update

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { (response) in
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }        }
     }
 
 
